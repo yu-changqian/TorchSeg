721ea1fea015f1716dbc330e5d6f3c9d1093f93a
ycszen <changqian_yu@163.com>
2019-01-23 16:33:37 +0800

    some util functions

---
+++ furnace/utils/init_func.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Time    : 2018/9/28 下午12:13
+# @Author  : yuchangqian
+# @Contact : changqian_yu@163.com
+# @File    : init_func.py.py
+import torch
+import torch.nn as nn
+
+
+def __init_weight(feature, conv_init, norm_layer, bn_eps, bn_momentum,
+                  **kwargs):
+    for name, m in feature.named_modules():
+        if isinstance(m, (nn.Conv2d, nn.Conv3d)):
+            conv_init(m.weight, **kwargs)
+        elif isinstance(m, norm_layer):
+            m.eps = bn_eps
+            m.momentum = bn_momentum
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
+
+def init_weight(module_list, conv_init, norm_layer, bn_eps, bn_momentum,
+                **kwargs):
+    if isinstance(module_list, list):
+        for feature in module_list:
+            __init_weight(feature, conv_init, norm_layer, bn_eps, bn_momentum,
+                          **kwargs)
+    else:
+        __init_weight(module_list, conv_init, norm_layer, bn_eps, bn_momentum,
+                      **kwargs)
+
+
+def group_weight(weight_group, module, norm_layer, lr):
+    group_decay = []
+    group_no_decay = []
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, (nn.Conv2d, nn.Conv3d)):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, norm_layer) or isinstance(m, nn.GroupNorm):
+            if m.weight is not None:
+                group_no_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+
+    assert len(list(module.parameters())) == len(group_decay) + len(
+        group_no_decay)
+    weight_group.append(dict(params=group_decay, lr=lr))
+    weight_group.append(dict(params=group_no_decay, weight_decay=.0, lr=lr))
+    return weight_group
---
+++ furnace/utils/pyt_utils.py
@@ -0,0 +1,161 @@
+# encoding: utf-8
+import os
+import sys
+import time
+import argparse
+from collections import OrderedDict, defaultdict
+
+import torch
+import torch.utils.model_zoo as model_zoo
+
+from engine.logger import get_logger
+
+logger = get_logger()
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+def load_model(model, model_file, is_restore=False):
+    t_start = time.time()
+    if isinstance(model_file, str):
+        state_dict = torch.load(model_file)
+        if 'model' in state_dict.keys():
+            state_dict = state_dict['model']
+    else:
+        state_dict = model_file
+    t_ioend = time.time()
+
+    if is_restore:
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = 'module.' + k
+            new_state_dict[name] = v
+        state_dict = new_state_dict
+
+    model.load_state_dict(state_dict, strict=False)
+    ckpt_keys = set(state_dict.keys())
+    own_keys = set(model.state_dict().keys())
+    missing_keys = own_keys - ckpt_keys
+    unexpected_keys = ckpt_keys - own_keys
+
+    if len(missing_keys) > 0:
+        logger.warning('Missing key(s) in state_dict: {}'.format(
+            ', '.join('{}'.format(k) for k in missing_keys)))
+
+    if len(unexpected_keys) > 0:
+        logger.warning('Unexpected key(s) in state_dict: {}'.format(
+            ', '.join('{}'.format(k) for k in unexpected_keys)))
+
+    del state_dict
+    t_end = time.time()
+    logger.info(
+        "Load model, Time usage:\n\tIO: {}, initialize parameters: {}".format(
+            t_ioend - t_start, t_end - t_ioend))
+
+    return model
+
+
+def parse_devices(input_devices):
+    if input_devices.endswith('*'):
+        devices = list(range(torch.cuda.device_count()))
+        return devices
+
+    devices = []
+    for d in input_devices.split(','):
+        if '-' in d:
+            start_device, end_device = d.split('-')[0], d.split('-')[1]
+            assert start_device != ''
+            assert end_device != ''
+            start_device, end_device = int(start_device), int(end_device)
+            assert start_device < end_device
+            assert end_device < torch.cuda.device_count()
+            for sd in range(start_device, end_device + 1):
+                devices.append(sd)
+        else:
+            device = int(d)
+            assert device < torch.cuda.device_count()
+            devices.append(device)
+
+    logger.info('using devices {}'.format(
+        ', '.join([str(d) for d in devices])))
+
+    return devices
+
+
+def extant_file(x):
+    """
+    'Type' for argparse - checks that file exists but does not open.
+    """
+    if not os.path.exists(x):
+        # Argparse uses the ArgumentTypeError to give a rejection message like:
+        # error: argument input: x does not exist
+        raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+    return x
+
+
+def link_file(src, target):
+    if os.path.isdir(target) or os.path.isfile(target):
+        os.remove(target)
+    os.system('ln -s {} {}'.format(src, target))
+
+
+def ensure_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+
+def _dbg_interactive(var, value):
+    from IPython import embed
+    embed()
+
+# def load_model(model, model_file):
+#     if isinstance(model_file, str):
+#         print('Load Model: ' + model_file)
+#         state_dict = torch.load(model_file)
+#     else:
+#         state_dict = model_file
+#
+#     from collections import OrderedDict
+#     new_state_dict = OrderedDict()
+#     for k, v in state_dict.items():
+#         name = k
+#         if k.split('.')[0] == 'module':
+#             name = k[7:]
+#         new_state_dict[name] = v
+#     model.load_state_dict(new_state_dict, strict=False)
+#
+#     return model
+#
+#
+# def parse_devices(input_devices):
+#     if input_devices.endswith('*'):
+#         devices = list(range(torch.cuda.device_count()))
+#         return devices
+#
+#     devices = []
+#     for d in input_devices.split(','):
+#         if '-' in d:
+#             start_device, end_device = d.split('-')[0], d.split('-')[1]
+#             assert start_device != ''
+#             assert end_device != ''
+#             start_device, end_device = int(start_device), int(end_device)
+#             assert start_device < end_device
+#             assert end_device < torch.cuda.device_count()
+#             for sd in range(start_device, end_device + 1):
+#                 devices.append(sd)
+#         else:
+#             device = int(d)
+#             assert device < torch.cuda.device_count()
+#             devices.append(device)
+#
+#     return devices
+
+#
+# def inspect(var):
+#     return CallbackInjector(var, _dbg_interactive)
