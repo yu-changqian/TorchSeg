49aadb6fb49592c753b11590eda83db376afdbcf
ycszen <changqian_yu@163.com>
2019-05-14 22:58:36 +0800

    support BiSeNet-R101

---
+++ model/bisenet/cityscapes.bisenet.R101/config.py
@@ -0,0 +1,114 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path as osp
+import sys
+import time
+import numpy as np
+from easydict import EasyDict as edict
+import argparse
+
+import torch.utils.model_zoo as model_zoo
+
+C = edict()
+config = C
+cfg = C
+
+C.seed = 12345
+
+"""please config ROOT_dir and user when u first using"""
+C.repo_name = 'TorchSeg'
+C.abs_dir = osp.realpath(".")
+C.this_dir = C.abs_dir.split(osp.sep)[-1]
+C.root_dir = C.abs_dir[:C.abs_dir.index(C.repo_name) + len(C.repo_name)]
+C.log_dir = osp.abspath(osp.join(C.root_dir, 'log', C.this_dir))
+C.log_dir_link = osp.join(C.abs_dir, 'log')
+C.snapshot_dir = osp.abspath(osp.join(C.log_dir, "snapshot"))
+
+exp_time = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+C.log_file = C.log_dir + '/log_' + exp_time + '.log'
+C.link_log_file = C.log_file + '/log_last.log'
+C.val_log_file = C.log_dir + '/val_' + exp_time + '.log'
+C.link_val_log_file = C.log_dir + '/val_last.log'
+
+"""Data Dir and Weight Dir"""
+C.dataset_path = "/root/Source/Datasets/Segmentation/Cityscapes/"
+C.img_root_folder = C.dataset_path
+C.gt_root_folder = C.dataset_path
+C.train_source = osp.join(C.dataset_path, "config_new/train.txt")
+C.eval_source = osp.join(C.dataset_path, "config_new/val.txt")
+C.test_source = osp.join(C.dataset_path, "config_new/test.txt")
+C.is_test = False
+
+"""Path Config"""
+
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+
+add_path(osp.join(C.root_dir, 'furnace'))
+
+from utils.pyt_utils import model_urls
+
+"""Image Config"""
+C.num_classes = 19
+C.background = -1
+C.image_mean = np.array([0.485, 0.456, 0.406])  # 0.485, 0.456, 0.406
+C.image_std = np.array([0.229, 0.224, 0.225])
+C.target_size = 1024
+C.image_height = 1024
+C.image_width = 1024
+C.num_train_imgs = 2975
+C.num_eval_imgs = 500
+
+""" Settings for network, this would be different for each kind of model"""
+C.fix_bias = True
+C.fix_bn = False
+C.sync_bn = True
+C.bn_eps = 1e-5
+C.bn_momentum = 0.1
+C.pretrained_model = "/root/Source/model_zoo/pytorch_model/resnet101_v1c.pth"
+
+"""Train Config"""
+C.lr = 1e-2
+C.lr_power = 0.9
+C.momentum = 0.9
+C.weight_decay = 5e-4
+C.batch_size = 16 #4 * C.num_gpu
+C.nepochs = 80
+C.niters_per_epoch = 1000
+C.num_workers = 24
+C.train_scale_array = [0.75, 1, 1.25, 1.5, 1.75, 2.0]
+
+"""Eval Config"""
+C.eval_iter = 30
+C.eval_stride_rate = 5 / 6
+C.eval_scale_array = [1, ]  # multi scales: 0.5, 0.75, 1, 1.25, 1.5, 1.75
+C.eval_flip = False
+C.eval_base_size = 1024
+C.eval_crop_size = 1024
+
+"""Display Config"""
+C.snapshot_iter = 50
+C.record_info_iter = 20
+C.display_iter = 50
+
+
+def open_tensorboard():
+    pass
+
+
+if __name__ == '__main__':
+    print(config.epoch_num)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-tb', '--tensorboard', default=False, action='store_true')
+    args = parser.parse_args()
+
+    if args.tensorboard:
+        open_tensorboard()
---
+++ model/bisenet/cityscapes.bisenet.R101/train.py
@@ -0,0 +1,160 @@
+from __future__ import division
+import os.path as osp
+import sys
+import argparse
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+import torch.backends.cudnn as cudnn
+# from torch.nn.parallel import DistributedDataParallel
+
+from config import config
+from dataloader import get_train_loader
+from network import BiSeNet
+from datasets import Cityscapes
+
+from utils.init_func import init_weight, group_weight
+from utils.pyt_utils import all_reduce_tensor
+from engine.lr_policy import PolyLR
+from engine.engine import Engine
+from seg_opr.loss_opr import SigmoidFocalLoss, ProbOhemCrossEntropy2d
+
+try:
+    from apex.parallel import DistributedDataParallel, SyncBatchNorm
+except ImportError:
+    raise ImportError(
+        "Please install apex from https://www.github.com/nvidia/apex .")
+
+parser = argparse.ArgumentParser()
+
+with Engine(custom_parser=parser) as engine:
+    args = parser.parse_args()
+
+    cudnn.benchmark = True
+
+    seed = config.seed
+    if engine.distributed:
+        seed = engine.local_rank
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+
+    # data loader
+    train_loader, train_sampler = get_train_loader(engine, Cityscapes)
+
+    # config network and criterion
+    # criterion = nn.CrossEntropyLoss(reduction='mean',
+    #                                 ignore_index=255)
+    min_kept = int(config.batch_size // len(
+        engine.devices) * config.image_height * config.image_width // 16)
+    criterion = ProbOhemCrossEntropy2d(ignore_label=255, thresh=0.7,
+                                       min_kept=min_kept,
+                                       use_weight=False)
+
+    if engine.distributed:
+        BatchNorm2d = SyncBatchNorm
+
+    model = BiSeNet(config.num_classes, is_training=True,
+                    criterion=criterion,
+                    pretrained_model=config.pretrained_model,
+                    norm_layer=BatchNorm2d)
+    init_weight(model.business_layer, nn.init.kaiming_normal_,
+                BatchNorm2d, config.bn_eps, config.bn_momentum,
+                mode='fan_in', nonlinearity='relu')
+
+    # group weight and config optimizer
+    base_lr = config.lr
+    # if engine.distributed:
+    #     base_lr = config.lr * engine.world_size
+
+    params_list = []
+    params_list = group_weight(params_list, model.context_path,
+                               BatchNorm2d, base_lr)
+    params_list = group_weight(params_list, model.spatial_path,
+                               BatchNorm2d, base_lr * 10)
+    params_list = group_weight(params_list, model.global_context,
+                               BatchNorm2d, base_lr * 10)
+    params_list = group_weight(params_list, model.arms,
+                               BatchNorm2d, base_lr * 10)
+    params_list = group_weight(params_list, model.refines,
+                               BatchNorm2d, base_lr * 10)
+    params_list = group_weight(params_list, model.heads,
+                               BatchNorm2d, base_lr * 10)
+    params_list = group_weight(params_list, model.ffm,
+                               BatchNorm2d, base_lr * 10)
+
+    optimizer = torch.optim.SGD(params_list,
+                                lr=base_lr,
+                                momentum=config.momentum,
+                                weight_decay=config.weight_decay)
+
+    # config lr policy
+    total_iteration = config.nepochs * config.niters_per_epoch
+    lr_policy = PolyLR(base_lr, config.lr_power, total_iteration)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    if engine.distributed:
+        model = DistributedDataParallel(model)
+
+    engine.register_state(dataloader=train_loader, model=model,
+                          optimizer=optimizer)
+    if engine.continue_state_object:
+        engine.restore_checkpoint()
+
+    model.train()
+
+    for epoch in range(engine.state.epoch, config.nepochs):
+        if engine.distributed:
+            train_sampler.set_epoch(epoch)
+        bar_format = '{desc}[{elapsed}<{remaining},{rate_fmt}]'
+        pbar = tqdm(range(config.niters_per_epoch), file=sys.stdout,
+                    bar_format=bar_format)
+        dataloader = iter(train_loader)
+        for idx in pbar:
+            optimizer.zero_grad()
+            engine.update_iteration(epoch, idx)
+
+            minibatch = dataloader.next()
+            imgs = minibatch['data']
+            gts = minibatch['label']
+
+            imgs = imgs.cuda(non_blocking=True)
+            gts = gts.cuda(non_blocking=True)
+
+            loss = model(imgs, gts)
+
+            # reduce the whole loss over multi-gpu
+            if engine.distributed:
+                reduce_loss = all_reduce_tensor(loss,
+                                                world_size=engine.world_size)
+
+            current_idx = epoch * config.niters_per_epoch + idx
+            lr = lr_policy.get_lr(current_idx)
+
+            for i in range(2):
+                optimizer.param_groups[0]['lr'] = lr
+            for i in range(2, len(optimizer.param_groups)):
+                optimizer.param_groups[i]['lr'] = lr * 10
+
+            loss.backward()
+            optimizer.step()
+            print_str = 'Epoch{}/{}'.format(epoch, config.nepochs) \
+                        + ' Iter{}/{}:'.format(idx + 1, config.niters_per_epoch) \
+                        + ' lr=%.2e' % lr \
+                        + ' loss=%.2f' % reduce_loss.item()
+
+            pbar.set_description(print_str, refresh=False)
+
+        if (epoch > config.nepochs - 20) or (epoch % config.snapshot_iter == 0):
+            if engine.distributed and (engine.local_rank == 0):
+                engine.save_and_link_checkpoint(config.snapshot_dir,
+                                                config.log_dir,
+                                                config.log_dir_link)
+            elif not engine.distributed:
+                engine.save_and_link_checkpoint(config.snapshot_dir,
+                                                config.log_dir,
+                                                config.log_dir_link)
