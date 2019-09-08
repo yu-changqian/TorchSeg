c6fb691a379139af357e9977a2b9f6d8fef2ba77
ycszen <changqian_yu@163.com>
2019-09-08 15:37:51 +0800

    add findcontours

--- furnace/utils/img_utils.py
+++ furnace/utils/img_utils.py
@@ -185,3 +185,20 @@ def normalize(img, mean, std):
     img = img / std
 
     return img
+
+
+def findContours(*args, **kwargs):
+    """
+    Wraps cv2.findContours to maintain compatiblity between versions 3 and 4
+    Returns:
+        contours, hierarchy
+    """
+    if cv2.__version__.startswith('4'):
+        contours, hierarchy = cv2.findContours(*args, **kwargs)
+    elif cv2.__version__.startswith('3'):
+        _, contours, hierarchy = cv2.findContours(*args, **kwargs)
+    else:
+        raise AssertionError(
+            'cv2 must be either version 3 or 4 to call this method')
+
+    return contours, hierarchy
