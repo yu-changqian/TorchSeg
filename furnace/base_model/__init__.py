ce802a0b1ae25576fd02d0c5499c04f9525b2aa8
ycszen <changqian_yu@163.com>
2019-12-03 14:58:02 +0800

    fix typos

--- furnace/base_model/__init__.py
+++ furnace/base_model/__init__.py
@@ -1,5 +1,2 @@
 from .resnet import ResNet, resnet18, resnet34, resnet50, resnet101, resnet152
 from .xception import Xception, xception39
-from .xsnet import XSNet, xsnet16, xsnet18, xsnet18v2, xsnet1x18, xsnet23, \
-    xsnet1x23, xsnet34, xsnet1x34
-from .xsnet_iw import iwxsnet18, iwxsnet1x34
