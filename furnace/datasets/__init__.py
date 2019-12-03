8c00d8a094e954adb0023b0530747b2365e06f80
ycszen <changqian_yu@163.com>
2019-12-03 15:18:56 +0800

    fix datasets

--- furnace/datasets/__init__.py
+++ furnace/datasets/__init__.py
@@ -1,8 +1,5 @@
 from .ade import ADE
 from .voc import VOC
 from .cityscapes import Cityscapes
-from .camvid import CamVid
-# from .stuff10k import Stuff10K
-from .pcontext import PascalContext
 
 __all__ = ['ADE', 'VOC', 'Cityscapes']
